675747a5139cd824748cfb6395f4a3fdaba00d28
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2018-11-23 16:47:41 +0800

    +): add images res

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      title: 'Flutter Demo', theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
